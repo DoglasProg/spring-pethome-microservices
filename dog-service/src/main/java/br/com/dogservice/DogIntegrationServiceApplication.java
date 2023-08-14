26206aa6c73f7291b2b4574903326beba9bd2b09
doglas.franca <doglas.franca@cwi.com.br>
2023-08-14 20:45:48 -0300

    start dog service

---
+++ dog-service/src/main/java/br/com/dogservice/DogIntegrationServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.dogservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogIntegrationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogIntegrationServiceApplication.class, args);
+	}
+
+}
